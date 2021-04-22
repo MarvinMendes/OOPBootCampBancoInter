850b2d7bc787346170a4c590963c0a5a7760d904
MarvinMendes <marvin.m.lopes@gmail.com>
2021-04-22 08:55:15 -0300

    Estudando conceitos do Parallel Stream - Um teste com recursividade

--- src/com/bootcamp/threads/TesteParallelStream.java
+++ src/com/bootcamp/threads/TesteParallelStream.java
@@ -19,6 +19,16 @@ public class TesteParallelStream {
         fim = System.currentTimeMillis();
         System.out.println("Tempo de execução com um Parallel Stream foi de: " + (fim - inicio));
 
+
+        /*com recursividade até um certo número de registros - testei com 1000 (mil) registros e foi mas performático com recursividade, uma casa decimal acima e já não se tornou mais viável -
+         vale a pena utilizar
+         mas se for um número alto vai estourar a pilha de execução
+        * */
+        System.out.println("Testando recursividade com parallel");
+        inicio = System.currentTimeMillis();
+        IntStream.range(1, 10000).parallel().forEach(num -> fatorialRecursivo(num));
+        fim = System.currentTimeMillis();
+        System.out.println("O tempo de execução de um Parallel em recursividade foi de: " + (fim -inicio));
     }
 
     public static long fatorial(long numero) {
@@ -28,4 +38,12 @@ public class TesteParallelStream {
         }
         return fatorial;
     }
+
+    public static long fatorialRecursivo(long numero) {
+        if (numero == 0 ) {
+            return 1;
+        }
+        return numero * fatorialRecursivo(numero -1);
+    }
+
 }
